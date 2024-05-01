5fd54c6fc15cabfe3f8160747d87a9a1d9c881dd
Tudzy <narongsak5az@gmail.com>
2024-05-01 14:24:40 +0700

    set reletionship

--- backend/models/index.js
+++ backend/models/index.js
@@ -34,6 +34,48 @@ db.role_tb = require('./role_tb.model')(sequelize, DataTypes)
 db.status_tb = require('./status_tb.model')(sequelize, DataTypes)
 db.case_tb = require('./case_tb.model')(sequelize, DataTypes)
 
+// one to one (one user have a one role) 
+db.user_tb.belongsTo(db.role_tb, {
+    foreignKey: 'roleId',
+    as: 'role_tbs'
+})
+db.role_tb.hasOne(db.user_tb, {
+    foreignKey: 'roleId',
+    as: 'user_tbs'
+})
+
+// one to many (one case have a many status)
+db.case_tb.belongsTo(db.status_tb, {
+    foreignKey: 'statusId',
+    as: 'status_tbs'
+})
+db.status_tb.hasMany(db.case_tb, {
+    foreignKey: 'statusId',
+    as: 'case_tbs'
+})
+
+// one to many (one tech has many cases)
+db.user_tb.hasMany(db.case_tb, {
+    foreignKey: 'techId',
+    as: 'tech_cases'
+});
+// one to many (one case belongs to one tech)
+db.case_tb.belongsTo(db.user_tb, {
+    foreignKey: 'techId',
+    as: 'case_tech'
+});
+
+// one to many (one user has many cases)
+db.user_tb.hasMany(db.case_tb, {
+    foreignKey: 'userId',
+    as: 'user_cases'
+});
+// one to many (one case belongs to one user)
+db.case_tb.belongsTo(db.user_tb, {
+    foreignKey: 'userId',
+    as: 'case_user'
+});
+
 sequelize.sync({ alter: true }).then(() => {
     console.log('Yes re-sync done!')
 })
